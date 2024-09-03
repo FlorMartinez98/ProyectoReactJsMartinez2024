4e070a6096c2c6a74fd11c5ea18bde99ca6fbe82
Florencia Martínez <f.martinezseh@gmail.com>
2024-09-03 01:25:52 -0300

    PrimeraEntrega1Martinez

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import ItemListContainer from './assets/componentes/ItemListContainer/ItemListContainer'
 import NavBar from './assets/componentes/NavBar/NavBar'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
--- src/assets/componentes/CartWidget/CartWidget.jsx
+++ src/assets/componentes/CartWidget/CartWidget.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./CartWidget.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 const CartWidget = () => {
   return (
-    <div className='cart-widget'>
-        <img className='imagenCarrito' src="https://i.pinimg.com/564x/73/f0/eb/73f0eb7475a4a7e66c3fc2e4d1a49bf0.jpg" alt="aarrito"/>
-        <strong>3</strong>
+    <div className="cart-widget">
+      <FontAwesomeIcon icon={faShoppingCart} size="lg" />
+      <strong>3</strong>
     </div>
   )
 }
