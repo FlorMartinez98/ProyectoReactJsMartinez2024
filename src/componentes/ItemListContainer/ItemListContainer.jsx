7853accaff03792fc7bbb35dc4fbdcc0c2ed9da1
Florencia Martínez <f.martinezseh@gmail.com>
2024-09-12 00:36:39 -0300

    PrimeraEntrega1Martinez

---
+++ src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useState, useEffect } from "react"
+import { getProductos } from "../../asynmock"
+import ItemList from "../ItemList/ItemList"
+
+
+const ItemListContainer = ({greeting}) => {
+
+    const [productos, setProductos] = useState([])
+
+    useEffect( ()=>{
+        getProductos()
+        .then(respuesta => setProductos(respuesta))
+        .catch(error => console.log(error))
+
+    }, [])
+
+return (
+
+    <>
+        <h2 style={{color:"pink"}}>{greeting}</h2>
+        <ItemList productos={productos}/>
+    
+    </>
+)
+}
+
+export default ItemListContainer
