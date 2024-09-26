77417e04217d6f88cb00e386bc4c3595f1e10424
Florencia Martínez <f.martinezseh@gmail.com>
2024-09-26 15:48:51 -0300

    PreEntrega2Martinez

--- src/componentes/ItemListContainer/ItemListContainer.jsx
+++ src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
-import { getProductos, getProductosPorCategorias } from "../../asyncmock";
 import ItemList from "../ItemList/IteamList";
 import { useParams } from "react-router-dom";
 import Loader from "../Loader/Loader";
+import { db } from "../../services/config";
+import { collection, getDocs, query, where } from "firebase/firestore";
+
 
 const ItemListContainer = () => {
     const [productos, setProductos] = useState([]);
@@ -10,20 +12,23 @@ const ItemListContainer = () => {
 
     const {idCategoria} = useParams()
 
-    useEffect(() => {
+    useEffect (()=> {
         setLoading(true)
-        const funcionProductos = idCategoria ? getProductosPorCategorias : getProductos;
+        const misProductos = idCategoria ? query(collection(db, "productos"), where("idCat", "==", idCategoria)) : (collection(db,"productos"))
 
-        funcionProductos(idCategoria)
-        .then(res => setProductos(res))
-        .catch((error) =>{
-            console.log(error)
+        getDocs(misProductos)
+        .then (res => {
+            const nuevosProductos = res.docs.map(doc =>{
+                const data = doc.data()
+                return {id:doc.id , ...data}
+            })
+            setProductos(nuevosProductos)
         })
+        .catch(error => console.log(error))
         .finally(()=>{
             console.log("proceso finalizado")
             setLoading(false)
         })
-        
     }, [idCategoria])
 
     return (
